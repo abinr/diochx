ef04875eab67026786ef486c3924c161ce27eb90
@abinr <brianfarrar@icloud.com>
2017-04-22 11:43:16 +0200

    Changed from conduit to wreq

--- app/Main.hs
+++ app/Main.hs
@@ -3,15 +3,17 @@
 module Main where
 
 import qualified System.Environment as OS
-import Network.HTTP.Simple
 import qualified Data.ByteString.Char8 as S8
+import Network.Wreq
+import Control.Lens
 
 main :: IO ()
 main = do
-  env <- OS.getEnv "TOKEN"
-  let
-    request =
-      setRequestHeader "Authorization" [S8.pack $ "Bearer " ++ env]
-      $ "GET https://api.digitalocean.com/v2/droplets"
-  response <- httpJSON request
-  putStrLn $ getResponseBody response
+  token <- OS.getEnv "TOKEN"
+  let opts =
+        defaults & header "Authorization" .~ [S8.pack $ "Bearer " ++ token]
+  r <- getWith opts "https://api.digitalocean.com/v2/droplets"
+  putStrLn $ show r
+
+
+
