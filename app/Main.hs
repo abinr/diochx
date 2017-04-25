3269080ce5a1353be4a3e0614723f1ef03d31661
@abinr <brianfarrar@icloud.com>
2017-04-25 10:42:35 +0200

    list droplet ids, json parsing

--- app/Main.hs
+++ app/Main.hs
@@ -6,14 +6,26 @@ import qualified System.Environment as OS
 import qualified Data.ByteString.Char8 as S8
 import Network.Wreq
 import Control.Lens
+import Data.Aeson
+import Data.Aeson.Lens
 
 main :: IO ()
 main = do
   token <- OS.getEnv "TOKEN"
   let opts =
         defaults & header "Authorization" .~ [S8.pack $ "Bearer " ++ token]
-  r <- getWith opts "https://api.digitalocean.com/v2/droplets"
-  putStrLn $ show r
+  r <- getWith opts $ url ++ "droplets"
+  print $ listDroplets $ r ^. responseBody
 
+listDroplets :: AsValue s => s -> Maybe [Integer]
+listDroplets json =
+  sequenceA
+  $ json ^.. key "droplets"
+  . _Array
+  . traverse
+  . to (^? key "id" . _Integer)
 
+url :: String
+url =
+  "https://api.digitalocean.com/v2/"
 
