25a9ec4b9bb9ef73bdf37648bc22158950a4ce39
@abinr <brianfarrar@icloud.com>
2017-05-07 10:58:06 +0200

    added droplet delete and create

--- app/Main.hs
+++ app/Main.hs
@@ -1,31 +1,54 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveGeneric #-}
 
 module Main where
 
 import qualified System.Environment as OS
 import qualified Data.ByteString.Char8 as S8
 import Network.Wreq
-import Control.Lens
+import Control.Lens hiding ((.=))
 import Data.Aeson
 import Data.Aeson.Lens
+import GHC.Generics (Generic)
 
 main :: IO ()
 main = do
   token <- OS.getEnv "TOKEN"
-  let opts =
-        defaults & header "Authorization" .~ [S8.pack $ "Bearer " ++ token]
-  r <- getWith opts $ url ++ "droplets"
-  print $ listDroplets $ r ^. responseBody
+  let opts = defaults
+         & header "Content-Type" .~ ["application/json"]
+         & header "Authorization" .~ [S8.pack . concat $ "Bearer " : lines token]
+  print opts
+  r <- getWith opts url
+  let ds = parseDroplets $ r ^. responseBody
+  print ds
+  r <- traverse (traverse (deleteWith opts . (url++) . show)) ds
+  r <- postWith opts url (toJSON createDroplet)
+  print r
+  print "end"
 
-listDroplets :: AsValue s => s -> Maybe [Integer]
-listDroplets json =
+
+parseDroplets :: AsValue s => s -> Maybe [Integer]
+parseDroplets json =
   sequenceA
   $ json ^.. key "droplets"
   . _Array
   . traverse
   . to (^? key "id" . _Integer)
 
+data Droplet =
+  Droplet {name :: String
+  , region :: String
+  , size :: String
+  , image :: String
+  } deriving (Generic, Show)
+  
+instance ToJSON Droplet
+
+createDroplet =
+  Droplet "ainur-lon" "lon1" "512mb" "ubuntu-17-04-x64"
+  
+
 url :: String
 url =
-  "https://api.digitalocean.com/v2/"
+  "https://api.digitalocean.com/v2/droplets/"
 
